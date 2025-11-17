54486936c768a7821004b68cdf643b105401c915
Shohei YOSHIDA <syohex@gmail.com>
2025-11-17 23:49:15 +0900

    Do not use curl and  use only C# library

--- src/VaTool.Lib/SokmilParser.cs
+++ src/VaTool.Lib/SokmilParser.cs
@@ -1,5 +1,4 @@
-﻿using System.Diagnostics;
-using AngleSharp;
+﻿using AngleSharp;
 using Microsoft.AspNetCore.WebUtilities;
 
 namespace VaTool.Lib;
@@ -10,38 +9,15 @@ public class SokmilParser : IParser
 
     public async Task<Product> Parse(string url, Config config)
     {
-        var processInfo = new ProcessStartInfo
-        {
-            FileName = "curl",
-            RedirectStandardOutput = true,
-            UseShellExecute = false,
-            CreateNoWindow = true,
-        };
+        using var client = new HttpClient();
+        client.DefaultRequestHeaders.Add("User-Agent", UserAgent);
+        client.DefaultRequestHeaders.Add("Cookie", "AGEAUTH=ok");
 
-        var curlArgs = new[]
-        {
-            "-A", UserAgent,
-            "--tlsv1.2",
-            "--tls-max", "1.2",
-            "-b", "AGEAUTH=ok",
-            "-L",
-            "-s",
-            url
-        };
-
-        foreach (var arg in curlArgs)
-        {
-            processInfo.ArgumentList.Add(arg);
-        }
-
-        var proc = new Process { StartInfo = processInfo };
-        proc.Start();
-
-        var output = await proc.StandardOutput.ReadToEndAsync();
-        await proc.WaitForExitAsync();
+        var response = await client.GetAsync(url);
+        var responseBody = await response.Content.ReadAsStringAsync();
 
         var context = BrowsingContext.New();
-        var document = await context.OpenAsync(req => req.Content(output).Address(url));
+        var document = await context.OpenAsync(req => req.Content(responseBody).Address(url));
 
         var product = new Product();
 
